b05fb4a7a0432109b9d9244ee17407ded88f2905
pcubillos <patricio.cubillos@oeaw.ac.at>
2016-02-22 00:12:35 +0100

    Bumped pyrat to version 1.2.5. Bumped lineread to version 6.4.1.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -8,9 +8,9 @@ PBAY_REV  = 14  # Revision
 # Pyrat version:
 PYRAT_VER =  1  # Major version
 PYRAT_MIN =  2  # Minor version
-PYRAT_REV =  4  # Revision
+PYRAT_REV =  5  # Revision
 
 # Lineread version:
 LR_VER    =  6  # Major version
 LR_MIN    =  4  # Minor version
-LR_REV    =  0  # Revision
+LR_REV    =  1  # Revision
