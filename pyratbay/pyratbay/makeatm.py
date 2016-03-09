8e5eb000647086fbc7957e5a975ecdc7b7d558ac
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2016-03-09 11:58:16 +0100

    In makeatm.py implemented:   makeatomic() to create atomic (elemental) abundance files.   readatomic() to read atomic (elemental) abundance files.   makepreatm() to create atomic (elemental) abundance profile files     (for TEA).   TEA2pyrat() to format a TEA atmospheric into Pyrat format. Added makecfg.py file with makeTEA() function to create a TEA config file. Added input files with Asplund solar abundances and TEA gdata default species-state names.

--- pyratbay/pyratbay/makeatm.py
+++ pyratbay/pyratbay/makeatm.py
@@ -1,6 +1,14 @@
+__all__ = ["uniform", "makeatomic", "readatomic", "makepreatm", "TEA2pyrat"]
+
+import os
 import numpy as np
 
-from .. import tools     as pt
+from .. import tools as pt
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+# Inputs dir
+indir   = thisdir + "/../../inputs/"
+
 
 def uniform(atmfile, pressure, temperature, species, abundances):
   """
@@ -58,3 +66,221 @@ def uniform(atmfile, pressure, temperature, species, abundances):
       # Species mole mixing ratios:
       f.write("  ".join(["{:12.6e}".format(ab) for ab in abundances]) + "\n")
   f.close()
+
+
+def makeatomic(solar, afile, xsolar=1.0, swap=None):
+  """
+  Generate an (atomic) elemental-abundances file by scaling the
+  solar abundances from Asplund et al. (2009).
+  http://adsabs.harvard.edu/abs/2009ARA%26A..47..481A
+
+  Parameters
+  ----------
+  solar: String
+     Input solar abundances filename.
+  afile: String
+     Output filename of modified atomic abundances.
+  xsolar: Integer
+     Multiplication factor for metal abundances (everything
+     except H and He).
+  swap: 2-element string tuple
+      Swap the abundances of the given elements.
+
+  Developers
+  ----------
+  Jasmina Blecic
+  Patricio Cubillos
+  """
+  # Read the Asplund et al. (2009) solar elementa abundances:
+  index, symbol, dex, name, mass = readatomic(solar)
+  # Count the number of elements:
+  nelements = len(symbol)
+
+  # Scale the metals aundances:
+  imetals = np.where((symbol != "H") & (symbol != "He"))
+  dex[imetals] += np.log10(xsolar)
+
+  # Swap abundances if requested:
+  if swap is not None:
+    dex0 = dex[np.where(symbol == swap[0])]
+    dex[np.where(symbol == swap[0])] = dex[np.where(symbol == swap[1])]
+    dex[np.where(symbol == swap[1])] = dex0
+
+  # Save data to file
+  with open(afile, "w") as f:
+    # Write header
+    f.write("# Elemental abundances:\n"
+            "# atomic number, symbol, dex abundance, name, molar mass (u).\n")
+    # Write data
+    for i in np.arange(nelements):
+      f.write("{:3d}  {:2s}  {:5.2f}  {:10s}  {:12.8f}\n".
+              format(index[i], symbol[i], dex[i], name[i], mass[i]))
+
+
+def readatomic(afile):
+  """
+  Read the solar elemental abundances from AsplundEtal2009.txt
+
+  Parameters
+  ----------
+  afile: String
+    Asplund file with solar elemental abundances.
+
+  Returns
+  -------
+  anum: 1D integer ndarray
+     Atomic number (except for Deuterium which has anum=0).
+  symbol: 1D string ndarray
+     Elemental chemical symbol.
+  dex: 1D float ndarray
+     Logarithmic number-abundance, scaled to log(H) = 12.
+  name: 1D string ndarray
+     Element names.
+  mass: 1D float ndarray
+     Elemental mass in amu.
+
+  Developers
+  ----------
+  Jasmina Blecic
+  Patricio Cubillos
+  """
+  # Allocate arrays:
+  nelements = 84  # Fixed number
+  anum   = np.zeros(nelements, np.int)
+  symbol = np.zeros(nelements, '|S2')
+  dex    = np.zeros(nelements, np.double)
+  name   = np.zeros(nelements, '|S20')
+  mass   = np.zeros(nelements, np.double)
+
+  # Open-read file:
+  with open(afile, 'r') as f:
+    # Read-discard first two lines (header):
+    f.readline()
+    f.readline()
+    # Store data into the arrays:
+    for i in np.arange(nelements):
+      anum[i], symbol[i], dex[i], name[i], mass[i] = f.readline().split()
+
+  return anum, symbol, dex, name, mass
+
+
+def makepreatm(pressure, temp, afile, elements, species, patm):
+  """
+  Generate a pre-atm file for TEA.
+  It converts dex abundances to number density
+  and divide them by hydrogen number density to get fractional abundances.
+  It writes the pressure, temperature, and elemental-abundances data
+  into a pre-atm file.
+
+  Parameters
+  ----------
+  pressure: String
+     Pressure atmospheric profile (bar).
+  abun_file: String
+     Name of the abundances file.
+  in_elem: String
+     String containing input elemental species.
+  out_spec: String
+     String containing output molecular species.
+  pre_atm: String
+     Pre-atmospheric filename.
+  temp: 1D float array
+     Temperature atmospheric profile (in K).
+
+  Developers
+  ----------
+  Jasmina Blecic
+  Patricio Cubillos
+  """
+  # Number of layers:
+  nlayers = len(pressure)
+
+  # Get the elemental abundace data:
+  index, symbol, dex, name, mass = readatomic(afile)
+  # Take only the elements we need:
+  iatoms = np.in1d(symbol, elements)
+
+  # Lisf of fractional number of molecules relative to hydrogen:
+  nfrac = (10.0**(dex-dex[np.where(symbol=="H")]))[iatoms].tolist()
+
+  # Write pre-atm file:
+  f = open(patm, 'w')
+
+  # Pre-atm header with basic instructions
+  f.write("# TEA pre-atmosphere file.\n"
+          "# Units: pressure (bar), temperature (K), abundance (unitless).\n\n")
+
+
+  # Load defaults:
+  sdefaults = {}
+  with open(indir+"TEA_gdata_defaults.txt", "r") as d:
+    for line in d:
+      sdefaults[line.split()[0]] = line.split()[1]
+
+  # Check species names:
+  for i in np.arange(len(species)):
+    if species[i].find("_") < 0:
+      species[i] = sdefaults[species[i]]
+
+  print(species)
+  # Write species names
+  f.write('#SPECIES\n{:s}\n\n'.format(" ".join(species)))
+
+  # Write TEA data:
+  f.write("#TEADATA\n#Pressure          Temp  " +
+          "  ".join(["{:>12s}".format(atom) for atom in symbol[iatoms]]) + "\n")
+  for i in np.arange(nlayers):
+    # Pressure, and Temperature:
+    f.write("{:10.4e}     {:>8.2f}  ".format(pressure[i], temp[i]))
+    # Elemental abundance list:
+    f.write("  ".join(["{:12.6e}".format(abun) for abun in nfrac]) + "\n")
+  f.close()
+
+
+def TEA2pyrat(teafile, atmfile):
+  """
+  Format a TEA output atmospheric file into a Pyrat atmospheric file.
+  """
+  # Open read the TEA file:
+  f = open(teafile, "r")
+  tea = np.asarray(f.readlines())
+  f.close()
+
+  ispec = np.where(tea == "#SPECIES\n")[0][0] + 1  # Species list
+  idata = np.where(tea == "#TEADATA\n")[0][0] + 2  # data starting line
+
+  # Read and clean species names:
+  species = tea[ispec].split()
+  nspecies = len(species)
+  for i in np.arange(nspecies):
+    species[i] = species[i].rpartition("_")[0]
+
+  # Write to file:
+  f = open(atmfile, "w")
+  f.write("# TEA atmospheric file formatted for Pyrat.\n\n")
+
+  # Set the values units:
+  f.write("# Abundance units (by number or mass):\n@ABUNDANCE\nnumber\n")
+  f.write("# Pressure units:\n@PRESSURE\nbar\n")
+  f.write("# Temperatures are always Kelvin.\n\n")
+
+  # Write the species names:
+  f.write("# Atmospheric composition:\n"
+          "@SPECIES\n" +
+          "  ".join(["{:<s}".format(mol) for mol in species]) + '\n\n')
+
+  # Write the per-layer data:
+  f.write("# Pressure  Temperature  " +
+        "".join(["{:<14s}".format(mol) for mol in species]) + "\n")
+  f.write("@DATA\n")
+
+  # For each layer write TEA data
+  nlayers = len(tea) - idata
+  for i in np.arange(nlayers):
+    data = np.asarray(tea[idata+i].split(), np.double)
+    # Pressure, and Temperature:
+    f.write("{:10.4e}  {:11.3f}  ".format(data[0], data[1]))
+    # Species mole mixing ratios:
+    f.write("  ".join(["{:12.6e}".format(ab) for ab in data[2:]]) + "\n")
+  f.close()
+  
---
+++ pyratbay/pyratbay/makecfg.py
@@ -0,0 +1,208 @@
+# Copyright (C) 2016 Patricio Cubillos. All rights reserved.
+# Pyrat Bay is open-source, reproducible-research code (see LICENSE).
+
+__all__ = ["makeTEA"]
+
+import os, sys
+import argparse, ConfigParser
+import numpy as np
+import scipy.constants as sc
+
+#import reader as rd
+#import constants as c
+
+pbdir  = os.path.dirname(os.path.realpath(__file__))
+TEAdir = os.path.realpath(pbdir + "/../../modules/TEA/")
+
+def makeTransit(cfile, tepfile, shareOpacity):
+  """
+  Make the transit configuration file.
+
+  Parameters:
+  -----------
+  cfile: String
+     BART configuration file.
+  tepfile: String
+     A TEP file.
+  """
+
+  # Known transit arguments:
+  known_args = ["radlow",  "radhigh",  "raddelt",  "radfct",
+                "wllow",   "wlhigh",   "wlfct",
+                "wnlow",   "wnhigh",   "wndelt",  "wnfct", "wnosamp",
+                "tlow",    "thigh",    "tempdelt",
+                "allowq",  "nwidth",
+                "opacityfile",
+                "molfile",
+                "toomuch", "tauiso", "outtau", "taulevel", "modlevel",
+                "starrad", "transparent",
+                "solution", "raygrid",
+                "gsurf", "refpress", "refradius",
+                "cloudrad", "cloudfct", "cloudext",
+                "verb",
+                "savefiles",
+                "outtoomuch", "outsample", "outmod", "outflux", "outintens",
+                "linedb", "csfile", "orbpars", "orbparsfct"]
+
+  # Name of the configuration-file section:
+  section = "MCMC"
+  # Read BART configuration file:
+  Bconfig = ConfigParser.SafeConfigParser()
+  Bconfig.read([cfile])
+
+  # Keyword names of the arguments in the BART configuration file:
+  args = Bconfig.options(section)
+
+  # transit configuration filename:
+  tcfile = open(Bconfig.get(section, "tconfig"), "w")
+
+  # Keyword for the atmospheric file is different in transit:
+  tcfile.write("atm {:s}\n".format(Bconfig.get(section, "atmfile")))
+
+  # Default molfile path:
+  if "molfile" not in args:
+    tcfile.write("molfile {:s}\n".format(
+      os.path.realpath(filedir + "/../modules/transit/inputs/molecules.dat")))
+
+  # Calculate gsurf and refradius from the tepfile:
+  tep = rd.File(tepfile)
+  mplanet = float(tep.getvalue('Mp')[0]) * c.Mjup
+  rplanet = float(tep.getvalue('Rp')[0]) * c.Rjup
+
+  # Planetary radius reference level in km:
+  Bconfig.set(section, "refradius", "{:.2f}".format(rplanet * 1e-3))
+  # Planetary surface gravity in cm/s2:
+  Bconfig.set(section, "gsurf", "{:.1f}".format(100*sc.G*mplanet/rplanet**2))
+  # Add these keywords:
+  args = np.union1d(args, ["refradius", "gsurf"])
+
+  # Reformat the cross-section inputs for Transit:
+  cs = Bconfig.get(section, "csfile")
+  Bconfig.set(section, "csfile", ",".join(cs.split()))
+
+  # Print the known arguments to file:
+  for key in np.intersect1d(args, known_args):
+    # FINDME: Why am I splitting?
+    values = Bconfig.get(section, key).split("\n")
+    for val in values:
+      tcfile.write("{:s} {:s}\n".format(key, val))
+
+  if shareOpacity:
+    tcfile.write("shareOpacity \n")
+  tcfile.close()
+
+
+def makeMCMC(cfile, MCMC_cfile, logfile):
+  """
+  Reformat configuration file to remove relative paths.  This output 
+  configuration file is used by the BART's MCMC program.
+
+  Parameters:
+  -----------
+  cfile: String
+     BART configuration file.
+  MCMC_cfile: String
+     Reformated configuration file.
+  logfile: String
+     Default logfile argument if not specified in cfile.
+  """
+
+  # Name of the configuration-file section:
+  section = "MCMC"
+  # Open input BART configuration file:
+  Bconfig = ConfigParser.SafeConfigParser()
+  Bconfig.optionxform = str
+  Bconfig.read([cfile])
+  # Keyword names of the arguments in the BART configuration file:
+  args = Bconfig.options(section)
+
+  # Known arguments that may have a path:
+  input_args = ["tep_name", "kurucz", "molfile", "filter", "linedb",
+                "csfile", "loc_dir"]
+  output_args = ["tconfig",    "atmfile",   "opacityfile", "press_file",
+                 "abun_basic", "abun_file", "preatm_file", "outflux",
+                 "outmod",     "savemodel", "logfile"]
+
+  # Set default logfile:
+  if "logfile" not in args:
+    Bconfig.set(section, "logfile", logfile)
+    args.append("logfile")
+
+  # Inputs should always exist:
+  for arg in np.intersect1d(args, input_args):
+    # Split multiple values (if more than one), get full path, join back:
+    values = Bconfig.get(section, arg).split("\n")
+    for v in np.arange(len(values)):
+      values[v] = os.path.realpath(values[v])
+    Bconfig.set(section, arg, "\n".join(values))
+
+  # Outputs are stored/copied into loc_dir:
+  for arg in np.intersect1d(args, output_args):
+    Bconfig.set(section, arg,
+                Bconfig.get(section, "loc_dir") + "/" +
+                os.path.basename(Bconfig.get(section, arg)))
+
+  # Add mpi:
+  Bconfig.set(section, "mpi", "True")
+
+  # Add func:
+  Bconfig.set(section, "func", "hack BARTfunc {:s}".format(filedir))
+
+  # Params is a special case:
+  params = Bconfig.get(section, "params")
+  # It may or not be a file path:
+  if os.path.isfile(params):
+    Bconfig.set(section, "params", os.path.realpath(params))
+
+  # Write the configuration file for use by MC3:
+  with open(MCMC_cfile, 'w') as configfile:
+    Bconfig.write(configfile)
+
+
+def makeTEA(cfile=None, maxiter=100, save_headers=False, save_outputs=False,
+            doprint=False, times=False, location_TEA=TEAdir, abun_file=None,
+            location_out="./TEA"):
+  """
+  Make a TEA configuration file.
+
+  Parameters
+  ----------
+  cfile: String
+     BART configuration file
+  TEAdir: String
+     Default TEA directory.
+  """
+  # Open New ConfigParser:
+  config = ConfigParser.SafeConfigParser()
+  config.add_section('TEA')
+  config.set("TEA", "maxiter",      str(maxiter))
+  config.set("TEA", "save_headers", str(save_headers))
+  config.set("TEA", "save_outputs", str(save_outputs))
+  config.set("TEA", "doprint",      str(doprint))
+  config.set("TEA", "times",        str(times))
+  config.set("TEA", "location_TEA", str(location_TEA))
+  config.set("TEA", "location_out", str(location_out))
+  config.set("TEA", "abun_file",    str(abun_file))
+
+  # Override with input ConfigParser values:
+  if cfile is not None:
+    Bconfig = ConfigParser.SafeConfigParser()
+    Bconfig.read([cfile])
+
+    keys = ["maxiter", "save_headers", "save_outputs", "doprint",
+            "times", "location_TEA", "abun_file", "location_out"]
+    # Set TEA default arguments:
+    for i in np.arange(len(keys)):
+      if Bconfig.has_option("PBAY", keys[i]):
+        config.set("TEA", keys[i], Bconfig.get("PBAY", keys[i]))
+
+  # For completion:
+  config.add_section('PRE-ATM')
+  config.set("PRE-ATM", "PT_file",        "None")
+  config.set("PRE-ATM", "pre_atm_name",   "None")
+  config.set("PRE-ATM", "input_elem",     "None")
+  config.set("PRE-ATM", "output_species", "None")
+
+  # Write TEA configuration file:
+  with open("TEA.cfg", 'w') as configfile:
+    config.write(configfile)
