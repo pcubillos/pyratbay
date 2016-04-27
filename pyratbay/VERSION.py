e8bb9e2bf76a7326fe94439125bdb2787db320ac
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2016-04-27 18:06:04 +0200

    Bumped Pyrat Bay to version 0.0.22. Bumped Pyrat to version 1.2.13. Bumped Lineread to version 6.4.5.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -3,14 +3,14 @@
 # Pyrat-Bay:
 PBAY_VER  =  0  # Major version
 PBAY_MIN  =  0  # Minor version
-PBAY_REV  = 21  # Revision
+PBAY_REV  = 22  # Revision
 
 # Pyrat version:
 PYRAT_VER =  1  # Major version
 PYRAT_MIN =  2  # Minor version
-PYRAT_REV = 12  # Revision
+PYRAT_REV = 13  # Revision
 
 # Lineread version:
 LR_VER    =  6  # Major version
 LR_MIN    =  4  # Minor version
-LR_REV    =  4  # Revision
+LR_REV    =  5  # Revision
