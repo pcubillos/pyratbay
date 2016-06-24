606266470e10cf6198404d7617be5be29b19a53e
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2016-06-24 17:44:05 +0200

    Bumped Pyrat Bay to version 0.0.25. Bumped Pyrat to version 1.2.16.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -3,12 +3,12 @@
 # Pyrat-Bay:
 PBAY_VER  =  0  # Major version
 PBAY_MIN  =  0  # Minor version
-PBAY_REV  = 24  # Revision
+PBAY_REV  = 25  # Revision
 
 # Pyrat version:
 PYRAT_VER =  1  # Major version
 PYRAT_MIN =  2  # Minor version
-PYRAT_REV = 15  # Revision
+PYRAT_REV = 16  # Revision
 
 # Lineread version:
 LR_VER    =  6  # Major version
