45b9033bb5cd8a6c4d29de197a4d45af29eed2ee
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2016-08-17 11:17:09 +0200

    Fixed routine to interpolate cross-sections using C splines code. New CS-interpolation routine is nearly 6 times faster and reproduces the previous results down to 0.03% difference. Removed Pyton 2D-interpolation routine. Added splinterp_2D() routine to spline.c (which in fact is a 1D interpolation that repeats along second axis). This resolves #18.

--- pyratbay/pyrat/crosssec.py
+++ pyratbay/pyrat/crosssec.py
@@ -3,8 +3,6 @@
 
 import sys, os
 import numpy as np
-import scipy.constants   as sc
-import scipy.interpolate as sip
 
 from .. import tools     as pt
 from .. import constants as pc
@@ -122,21 +120,21 @@ def read(pyrat):
         iabsorp[j] = sp.splinterp(absorption[j], wavenumber, pyrat.spec.wn, 0.0)
       pyrat.cs.iabsorp.append(iabsorp)
       # Array with second derivatives:
-      # Note the shape is swapped w.r.t. iabsorp.
-      iz      = np.zeros((pyrat.spec.nwave, pyrat.cs.ntemp[i]), np.double)
-      wnlow = np.flatnonzero(pyrat.spec.wn >= wavenumber[ 0])[ 0]
-      wnhi  = np.flatnonzero(pyrat.spec.wn <= wavenumber[-1])[-1]
-      for j in np.arange(wnlow, wnhi):   # FINDME: wnhi+1 ??
+      iz   = np.zeros((pyrat.spec.nwave, pyrat.cs.ntemp[i]), np.double)
+      wnlo = np.flatnonzero(pyrat.spec.wn >= wavenumber[ 0])[ 0]
+      wnhi = np.flatnonzero(pyrat.spec.wn <= wavenumber[-1])[-1]
+      for j in np.arange(wnlo, wnhi):
         iz[j] = sp.spline_init(iabsorp[:,j], pyrat.cs.temp[i])
-      pyrat.cs.iz.append(iz)
+      pyrat.cs.iz.append(iz.T)
+      pyrat.cs.iwnlo.append(wnlo)
+      pyrat.cs.iwnhi.append(wnhi)
 
   pt.msg(pyrat.verb-3, "Done.", pyrat.log)
 
 
-def tmp_interpolate(pyrat):
+def interpolate(pyrat):
   """
-  Interpolate the CS absorption to the planetary model temperature and
-  wavenumber samples.
+  Interpolate the CS absorption into the planetary model temperature.
   """
   pt.msg(pyrat.verb-3, "\nBegin CS interpolation.", pyrat.log)
 
@@ -157,68 +155,8 @@ def tmp_interpolate(pyrat):
 
   for i in np.arange(pyrat.cs.nfiles):
     cs_absorption = np.zeros((pyrat.atm.nlayers, pyrat.spec.nwave))
-    wnlow = np.flatnonzero(pyrat.spec.wn >= pyrat.cs.wavenumber[i][ 0])[ 0]
-    wnhi  = np.flatnonzero(pyrat.spec.wn <= pyrat.cs.wavenumber[i][-1])[-1]
-    for p in np.arange(pyrat.atm.nlayers):
-      for wn in np.arange(wnlow, wnhi):
-        cs_absorption[p,wn] = sp.splinterp_pt(pyrat.cs.iabsorp[i],
-                                    pyrat.cs.temp[i], pyrat.cs.iz[i][wn],
-                                    pyrat.cs.ntemp[i], pyrat.atm.temp[p])
-
-    # Apply density scale factor:
-    dens = 1.0
-    for j in np.arange(pyrat.cs.nmol[i]):
-      # Get index from the pyrat list of molecules:
-      imol = np.where(pyrat.mol.name == pyrat.cs.molecules[i,j])[0][0]
-      # Densities in amagat:
-      dens *= pyrat.atm.d[:,imol] / pc.amagat
-
-    # Compute CS absorption in cm-1 units (broadcasting):
-    pyrat.cs.ec += (cs_absorption * np.expand_dims(dens, axis=1))
-
-    pt.msg(pyrat.verb-6, "CS extinction: {}".format(pyrat.cs.ec[:,0]),
-           pyrat.log, 2)
-  pt.msg(pyrat.verb-3, "Done.", pyrat.log)
-
-
-def interpolate(pyrat):
-  """
-  Interpolate the CS absorption to the planetary model temperature and
-  wavenumber samples.
-  """
-  pt.msg(pyrat.verb-3, "\nBegin CS interpolation.", pyrat.log)
-
-  # Check temperature boundaries:
-  if np.any(pyrat.atm.temp < pyrat.cs.tmin):
-    icold = np.where(pyrat.atm.temp < pyrat.cs.tmin)[0][0]
-    pt.error("The layer {:d} in the atmospheric model has a lower temperature "
-             "({:.1f} K) than the lowest allowed CS temperature ({:.1f} K).".
-              format(icold, pyrat.atm.temp[icold], pyrat.cs.tmin), pyrat.log)
-  if np.any(pyrat.atm.temp > pyrat.cs.tmax):
-    ihot = np.where(pyrat.atm.temp > pyrat.cs.tmax)[0][0]
-    pt.error("The layer {:d} in the atmospheric model has a higher temperature "
-             "({:.1f} K) than the highest allowed CS temperature ({:.1f} K).".
-              format(ihot, pyrat.atm.temp[ihot], pyrat.cs.tmax), pyrat.log)
-
-  # Allocate output extinction-coefficient array:
-  pyrat.cs.ec = np.zeros((pyrat.atm.nlayers, pyrat.spec.nwave))
-
-  # Interpolator will require sorted arrays:
-  asort  = np.argsort(pyrat.atm.temp)
-  # This one will de-sort to the original order:
-  desort = np.argsort(asort)
-  sort_temp = pyrat.atm.temp[asort]
-
-  for i in np.arange(pyrat.cs.nfiles):
-    # Evaluate the spline:
-    biv = sip.RectBivariateSpline(pyrat.cs.temp[i],
-                                  pyrat.cs.wavenumber[i],
-                                  pyrat.cs.absorption[i])
-
-    # Interpolate:
-    cs_absorption = biv(sort_temp, pyrat.spec.wn)
-    # Reverse sorting to the original order of the atmospheric layers:
-    cs_absorption = cs_absorption[desort]
+    sp.splinterp_2D(pyrat.cs.iabsorp[i], pyrat.cs.temp[i], pyrat.cs.iz[i],
+        pyrat.atm.temp, cs_absorption, pyrat.cs.iwnlo[i], pyrat.cs.iwnhi[i])
 
     # Apply density scale factor:
     dens = 1.0
