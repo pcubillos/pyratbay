Fixed bug iscale and ibulk computation.  The indices were not sorted acording to the molscale and bulk species list.

pyratbay/atmosphere/qscale.py
@@ -123,9 +123,13 @@ def qscale(Q, spec, qscale, molscale, bulk, qsat=None,
   """
   q = np.copy(Q)
   if iscale is None:
-    iscale = np.where(np.in1d(spec, molscale))[0]
+    iscale = []
+    for mol in molscale:
+      iscale += list(np.where(spec==mol)[0])
   if ibulk is None:
-    ibulk = np.where(np.in1d(spec, bulk))[0]
+    ibulk = []
+    for mol in bulk:
+      ibulk  += list(np.where(spec==mol)[0])
   if bratio is None:
     bratio, invsrat = ratio(Q, ibulk)
 

pyratbay/pyrat/argum.py
@@ -871,10 +871,14 @@ def setup(pyrat):
 
   # Obtain abundance ratios between the bulk species:
   if ret.bulk is not None:
-    ret.ibulk  = np.where(np.in1d(species, ret.bulk))[0]
+    ret.ibulk = []
+    for mol in ret.bulk:
+      ret.ibulk  += list(np.where(species==mol)[0])
     ret.bulkratio, ret.invsrat = atm.ratio(pyrat.atm.q, ret.ibulk)
   if ret.molscale is not None:
-    ret.iscale = np.where(np.in1d(species, ret.molscale))[0]
+    ret.iscale = []
+    for mol in ret.molscale:
+      ret.iscale += list(np.where(species==mol)[0])
     nabund = len(ret.iscale)
   else:
     nabund = 0