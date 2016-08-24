Fixed bug in haze extinction-coefficient calculation: In haze.py, moved extinction() function  into absorption() such that the model parameters get updated in each driver's run() call. Updated driver.py timestamps to time the haze and alkali models.

pyratbay/pyrat/driver.py
@@ -90,9 +90,6 @@ def init(argv, main=False):
   cs.read(pyrat)
   timestamps.append(time.time())
 
-  # Calculate haze opacity cross section:
-  hz.extinction(pyrat)
-
   # Calculate extinction-coefficient table:
   ex.exttable(pyrat)
   timestamps.append(time.time())
@@ -123,9 +120,13 @@ def run(pyrat, inputs=None):
   cs.interpolate(pyrat)
   timestamps.append(time.time())
 
-  # Calculate the haze and alkali absorption:
+  # Calculate the haze absorption:
   hz.absorption(pyrat)
+  timestamps.append(time.time())
+
+  # Calculate the alkali absorption:
   al.absorption(pyrat)
+  timestamps.append(time.time())
 
   # Calculate the optical depth:
   od.opticaldepth(pyrat)
@@ -136,13 +137,14 @@ def run(pyrat, inputs=None):
   timestamps.append(time.time())
 
   pyrat.timestamps += list(np.ediff1d(timestamps))
-  dtime = pyrat.timestamps[0:9] + pyrat.timestamps[-4:]
+  dtime = pyrat.timestamps[0:9] + pyrat.timestamps[-6:]
   pt.msg(pyrat.verb-4, "\nTimestamps:\n"
         " Init:     {:10.6f}\n Parse:    {:10.6f}\n Inputs:   {:10.6f}\n"
         " Wnumber:  {:10.6f}\n Atmosph:  {:10.6f}\n TLI:      {:10.6f}\n"
         " Layers:   {:10.6f}\n Voigt:    {:10.6f}\n CIA read: {:10.6f}\n"
-        " Extinct:  {:10.6f}\n CIA intp: {:10.6f}\n O.Depth:  {:10.6f}\n"
-        " Spectrum: {:10.6f}".format(*dtime), pyrat.log)
+        " Extinct:  {:10.6f}\n CIA intp: {:10.6f}\n Haze:     {:10.6f}\n"
+        " Alkali:   {:10.6f}\n O.Depth:  {:10.6f}\n Spectrum: {:10.6f}".
+        format(*dtime), pyrat.log)
 
   if len(pyrat.wlog) > 0:
     # Write all warnings to file:

pyratbay/pyrat/haze.py
@@ -7,17 +7,6 @@ import numpy as np
 from .. import tools as pt
 from .. import constants as pc
 
-def extinction(pyrat):
-  """
-  Calculate the hazes extinction coefficient.
-  """
-  # Get list of haze models
-  for i in np.arange(pyrat.haze.nmodels):
-    # Feed params into the model:
-    pass
-    # Calculate the extinction coefficient (in cm2 molecule-1)
-    pyrat.haze.model[i].extinction(pyrat.spec.wn, pyrat.atm.press)
-
 
 def absorption(pyrat):
   """
@@ -27,6 +16,9 @@ def absorption(pyrat):
   pyrat.haze.ec = np.zeros((pyrat.atm.nlayers, pyrat.spec.nwave))
 
   for i in np.arange(pyrat.haze.nmodels):
+    # Calculate the extinction coefficient (in cm2 molecule-1):
+    pyrat.haze.model[i].extinction(pyrat.spec.wn, pyrat.atm.press)
+
     # Get molecule index:
     imol = np.where(pyrat.mol.name == pyrat.haze.model[i].mol)[0][0]
     # Densities in molecules cm-3: