pcubillos/pyratbay

commit 5741a63217b872a009506b2985046b9d41524258

Check and reject MCMC proposals that generate an atmospheric model where the top radius layer cannot reach the data Rp/Rs (for transmission geometry).

pyratbay/pbay/pyratfit.py
@@ -64,9 +64,11 @@ def fit(params, pyrat, freeze=False):
 
   # Update temperature profile:
   temp = pyrat.ret.tmodel(params[pyrat.ret.itemp], *pyrat.ret.targs)
+  # Turn-on out-of-bounds temperature flag:
   if np.any(temp < pyrat.ret.tlow) or np.any(temp > pyrat.ret.thigh):
-    pyrat.obs.bandflux[:] = -1e10  # FINDME: what if np.inf? or nan?
-    return pyrat.obs.bandflux
+    temp[:] = 0.5*(pyrat.ret.tlow + pyrat.ret.thigh)
+    tempflag = True
+
   # Update abundance profiles:
   q2 = atm.qscale(pyrat.atm.q, pyrat.mol.name, params[pyrat.ret.iabund],
                   pyrat.ret.molscale, pyrat.ret.bulk,
@@ -92,6 +94,11 @@ def fit(params, pyrat, freeze=False):
 
   # Band-integrate spectrum:
   pyrat.obs.bandflux = w.bandintegrate(pyrat=pyrat)
+
+  # Reject this iteration if there are invalid temperatures or radii:
+  if tempflag or (pyrat.od.path == "transit" and
+      np.any(pyrat.obs.data > (pyrat.atm.radius[0]/pyrat.phy.rstar)**2)):
+    pyrat.obs.bandflux[:] = np.inf
 
   # Revert changes in the atmospheric profile:
   if freeze:

commit b56cc2d22962b09b74247c910482fdcf4c45ca65

Fixed bug in pyratfit() MCMC fitting function, introduced in 5741a63. The default value of the rejection flag was missing.

pyratbay/pbay/pyratfit.py
@@ -62,18 +62,20 @@ def fit(params, pyrat, freeze=False):
   if freeze:
     t0, q0, r0 = pyrat.atm.temp, pyrat.atm.q, pyrat.atm.radius
 
+  rejectflag = False
   # Update temperature profile:
   temp = pyrat.ret.tmodel(params[pyrat.ret.itemp], *pyrat.ret.targs)
-  # Turn-on out-of-bounds temperature flag:
+  # Turn-on reject flag if out-of-bounds temperature:
   if np.any(temp < pyrat.ret.tlow) or np.any(temp > pyrat.ret.thigh):
     temp[:] = 0.5*(pyrat.ret.tlow + pyrat.ret.thigh)
-    tempflag = True
+    rejectflag = True
 
   # Update abundance profiles:
   q2 = atm.qscale(pyrat.atm.q, pyrat.mol.name, params[pyrat.ret.iabund],
                   pyrat.ret.molscale, pyrat.ret.bulk,
                   iscale=pyrat.ret.iscale, ibulk=pyrat.ret.ibulk,
                   bratio=pyrat.ret.bulkratio, invsrat=pyrat.ret.invsrat)
+
   # Update radius profile:
   if len(pyrat.ret.irad) > 0:
     radius = pyrat.hydro(pyrat.atm.press, temp, pyrat.atm.mm,
@@ -81,6 +83,7 @@ def fit(params, pyrat, freeze=False):
                          pyrat.refpressure, params[pyrat.ret.irad][0]*pc.km)
   else:
     radius = None
+
   # Update haze parameters:
   if len(pyrat.ret.ihaze) > 0:
     j = 0
@@ -95,9 +98,14 @@ def fit(params, pyrat, freeze=False):
   # Band-integrate spectrum:
   pyrat.obs.bandflux = w.bandintegrate(pyrat=pyrat)
 
+  # Turn-on reject flag if it doesn't cover the transit-depth values:
+  if (pyrat.od.path == "transit" and
+      np.any(pyrat.obs.data >
+             (pyrat.atm.radius[pyrat.atm.rtop]/pyrat.phy.rstar)**2)):
+    rejectflag = True
+
   # Reject this iteration if there are invalid temperatures or radii:
-  if tempflag or (pyrat.od.path == "transit" and
-      np.any(pyrat.obs.data > (pyrat.atm.radius[0]/pyrat.phy.rstar)**2)):
+  if rejectflag:
     pyrat.obs.bandflux[:] = np.inf
 
   # Revert changes in the atmospheric profile: