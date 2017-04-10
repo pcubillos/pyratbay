7366d0b2a6b28af4791d6ef9c7a6c9f9fb78f243
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2017-04-10 18:09:03 +0200

    Fixed bug in Exomol linereader (wrong Elow index). Generalized name, molecule, and isotope names extraction. Updated documentation. Currently tested for HCN database.

--- pyratbay/lineread/db/exomol.py
+++ pyratbay/lineread/db/exomol.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2016-2017 Patricio Cubillos and contributors.
 # Pyrat Bay is currently proprietary software (see LICENSE).
 
-__all__ = ["hitran"]
+__all__ = ["exomol"]
 
 import os
 import re
@@ -10,9 +10,6 @@ import numpy as np
 from ... import tools     as pt
 from ... import constants as pc
 from .driver import dbdriver
-
-# Directory of db:
-DBdir = os.path.dirname(os.path.realpath(__file__))
 
 
 class exomol(dbdriver):
@@ -23,12 +20,13 @@ class exomol(dbdriver):
     Parameters
     ----------
     dbfile: String
-       File with the Database info as given from HITRAN.
+       File with the Database info as given from Exomol.
     pffile: String
        File with the partition function.
+    log: FILE
+       A log file.
     """
     super(exomol, self).__init__(dbfile, pffile)
-
     self.recwnpos  =  37 # Wavenumber position in record
     self.recwnlen  =  12 # Wavenumber record length
 
@@ -39,26 +37,26 @@ class exomol(dbdriver):
     with open(dbfile.replace(".trans", ".states"), "r") as f:
       lines = f.readlines()
     nstates = len(lines)
-    self.stateID = np.zeros(nstates, int)
     self.E       = np.zeros(nstates, np.double)
     self.J       = np.zeros(nstates, int)
     for i in np.arange(nstates):
-      self.stateID[i], self.E[i], self.J[i] = lines[i].split()[:3]
+      self.E[i], self.J[i] = lines[i].split()[1:3]
 
     # Get info from file name:
     s = os.path.split(dbfile)[1].split("_")[0].split("-")
     self.molecule = ""
-    isotopes = ""
+    isotopes      = ""
     mass = 0.0
     for i in np.arange(len(s)):
-      match = re.match(r"([0-9]+(?:.[0-9]+)?)([a-z]+)", s[i], re.I)
-      self.molecule += match.group(2)
-      isotopes += match.group(1)[-1:]
-      mass += float(match.group(1))
+      match = re.match(r"([0-9]+)([a-z]+)([0-9]*)", s[i], re.I)
+      N = 1 if match.group(3) == "" else int(match.group(3))
+      self.molecule += match.group(2) + match.group(3)
+      isotopes += match.group(1)[-1:] * N
+      mass += float(match.group(1))*N
     self.iso = isotopes
     self.isotopes = [isotopes]
-    self.mass     = [mass]  # get somehow
-    self.isoratio = [1.0]  # ditto
+    self.mass     = [mass]
+    self.isoratio = [1.0]   # ditto
     # Database name:
     self.name = "Exomol " + self.molecule
 
@@ -89,20 +87,18 @@ class exomol(dbdriver):
 
   def dbread(self, iwn, fwn, verb, *args):
     """
-    Read a HITRAN or HITEMP database (dbfile) between wavenumbers iwn and fwn.
+    Read an Exomol database (dbfile) between wavenumbers iwn and fwn.
 
     Parameters
     ----------
     dbfile: String
-       A HITRAN or HITEMP database filename.
+       An Exomol line-list database filename.
     iwn: Float
        Initial wavenumber limit (in cm-1).
     fwn: Float
        Final wavenumber limit (in cm-1).
     verb: Integer
        Verbosity threshold.
-    pffile: String
-       Partition function filename.
 
     Returns
     -------
@@ -117,11 +113,10 @@ class exomol(dbdriver):
 
     Notes
     -----
-    - The HITRAN data is provided in ASCII format.
-    - The line transitions are sorted in increasing wavenumber (cm-1) order.
+      The line transitions are sorted in increasing wavenumber (cm-1) order.
     """
 
-    # Open HITRAN file for reading:
+    # Open file for reading:
     data = open(self.dbfile, "r")
 
     # Read first line to get the record size:
@@ -136,15 +131,11 @@ class exomol(dbdriver):
     # Find the record index for iwn and fwn:
     istart = self.binsearch(data, iwn, 0,      nlines-1, 0)
     istop  = self.binsearch(data, fwn, istart, nlines-1, 1)
-    #print(istart, istop)
 
     # Non-overlaping wavenumber ranges:
-    data.seek(self.recwnpos)
-    DBiwn = float(data.read(self.recwnlen))
+    DBiwn = self.readwave(data, 0)
+    DBfwn = self.readwave(data, nlines-1)
 
-    data.seek((nlines-1) * self.recsize + self.recwnpos)
-    DBfwn = float(data.read(self.recwnlen))
-    #print(DBiwn, DBfwn)
     if iwn > DBfwn or fwn < DBiwn:
       pt.warning(verb-2, "Database ('{:s}') wavenumber range ({:.2f}--{:.2f} "
         "cm-1) does not overlap with the requested wavenumber range "
@@ -154,7 +145,6 @@ class exomol(dbdriver):
 
     # Number of records to read:
     nread = istop - istart + 1
-    print(nread)
     # Allocate arrays for values to extract:
     wnumber = np.zeros(nread, np.double)
     gf      = np.zeros(nread, np.double)
@@ -177,23 +167,21 @@ class exomol(dbdriver):
       upID[i], loID[i], A21[i], wnumber[i] = line.split()
       # Print a checkpoint statement every 10% interval:
       if (i % interval) == 0.0  and  i != 0:
-        #gfval = A21[i]*g2[i]*pc.C1/(8.0*np.pi*pc.c)/wnumber[i]**2.0
         pt.msg(verb-4, "{:5.1f}% completed.".format(10.*i/interval),
                self.log, 3)
-        #pt.msg(verb-5,"Wavenumber: {:8.2f} cm-1   Wavelength: {:6.3f} um\n"
-        #                "Elow:     {:.4e} cm-1   gf: {:.4e}   Iso ID: {:2d}".
-        #                 format(wnumber[i], 1.0/(wnumber[i]*pc.um),
-        #                        elow[i], gfval, (isoID[i]-1)%10), self.log, 6)
+        gfval=(2*self.J[upID[i]-1]+1)*A21[i]*pc.C1/(8*np.pi*pc.c)/wnumber[i]**2
+        pt.msg(verb-5,"Wavenumber: {:8.2f} cm-1   Wavelength: {:6.3f} um\n"
+                        "Elow:     {:.4e} cm-1   gf: {:.4e}   Iso ID: {:2d}".
+                         format(wnumber[i], 1.0/(wnumber[i]*pc.um),
+                                self.E[loID[i]-1], gfval,
+                                self.isotopes.index(self.iso), self.log, 6))
       i += 1
 
 
     # Calculate gf using Equation (4) of Harris et al. (2006):
-    gf = (2*self.J[upID]+1) * A21 * pc.C1 / (8.0 * np.pi * pc.c) / wnumber**2.0
-    # Elow:
-    elow = self.E[loID]
-    # Isotopic index:
+    gf[:] = (2*self.J[upID-1]+1) * A21 * pc.C1 / (8.0*np.pi*pc.c) / wnumber**2.0
+    elow[:] = self.E[loID-1]
     isoID[:] = self.isotopes.index(self.iso)
     data.close()
 
-    # Remove lines with unknown Elow, see Rothman et al. (1996):
     return wnumber, gf, elow, isoID
