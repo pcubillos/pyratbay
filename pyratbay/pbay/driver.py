80da4fc17649f815c9fc7e837e2c72802f274e1b
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-01-25 16:36:46 +0100

    Adjusted retrieval call to work with latest changes in pbay.fit().

--- pyratbay/pbay/driver.py
+++ pyratbay/pbay/driver.py
@@ -138,9 +138,11 @@ def run(argv, main=False):
     # Basename of the output files:
     outfile = os.path.splitext(os.path.basename(log.name))[0]
     # Run MCMC:
+    freeze   = True  # Freeze abundances evoer iterations
+    retmodel = False # Return only the band-integrated spectrum
     bestp, CRlo, CRhi, stdp, posterior, Zchain = mc3.mcmc(
            data=args.data, uncert=args.uncert,
-           func=pf.fit, indparams=[pyrat, True], params=args.params,
+           func=pf.fit, indparams=[pyrat,freeze,retmodel], params=args.params,
            pmin=args.pmin, pmax=args.pmax, stepsize=args.stepsize,
            prior=args.prior, priorlow=args.priorlow, priorup=args.priorup,
            walk=args.walk, nsamples=args.nsamples, nchains=args.nchains,
