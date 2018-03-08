51796fea9b3720db1e5a2134bde10ae1c628b87b
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-03-08 13:45:48 +0100

    Updated makeTEA() to work with multiprocessing TEA version.

--- pyratbay/pbay/makecfg.py
+++ pyratbay/pbay/makecfg.py
@@ -12,9 +12,9 @@ pbdir  = os.path.dirname(os.path.realpath(__file__))
 TEAdir = os.path.realpath(pbdir + "/../../modules/TEA/")
 
 
-def makeTEA(cfile=None, maxiter=100, save_headers=False, save_outputs=False,
-            doprint=False, times=False, location_TEA=TEAdir, abun_file=None,
-            location_out="./TEA"):
+def makeTEA(cfile=None, maxiter=100, savefiles=False,
+            verb=1, times=False, abun_file=None,
+            location_out="./TEA", guess=None, ncpu=1):
   """
   Make a TEA configuration file.
 
@@ -29,11 +29,11 @@ def makeTEA(cfile=None, maxiter=100, save_headers=False, save_outputs=False,
   config = cp.SafeConfigParser()
   config.add_section('TEA')
   config.set("TEA", "maxiter",      str(maxiter))
-  config.set("TEA", "save_headers", str(save_headers))
-  config.set("TEA", "save_outputs", str(save_outputs))
-  config.set("TEA", "doprint",      str(doprint))
+  config.set("TEA", "savefiles",    str(savefiles))
+  config.set("TEA", "verb",         str(verb))
   config.set("TEA", "times",        str(times))
-  config.set("TEA", "location_TEA", str(location_TEA))
+  config.set("TEA", "guess",        str(guess))
+  config.set("TEA", "ncpu",         str(ncpu))
   config.set("TEA", "location_out", str(location_out))
   config.set("TEA", "abun_file",    str(abun_file))
 
@@ -42,8 +42,8 @@ def makeTEA(cfile=None, maxiter=100, save_headers=False, save_outputs=False,
     Bconfig = cp.SafeConfigParser()
     Bconfig.read([cfile])
 
-    keys = ["maxiter", "save_headers", "save_outputs", "doprint",
-            "times", "location_TEA", "abun_file", "location_out"]
+    keys = ["maxiter", "savefiles", "verb",
+            "times", "location_TEA", "abun_file", "ncpu", "location_out"]
     # Set TEA default arguments:
     for i in np.arange(len(keys)):
       if Bconfig.has_option("PBAY", keys[i]):
