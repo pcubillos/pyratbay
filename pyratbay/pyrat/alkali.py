e45c31737bdd5be24f07604fbcdf40428503e8bf
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-06-27 13:31:34 +0200

    Added clarification in the line-intensity equation in alkali.py about the neglected terms.

--- pyratbay/pyrat/alkali.py
+++ pyratbay/pyrat/alkali.py
@@ -82,6 +82,8 @@ def absorption(pyrat):
       alkali.ec += (pc.C3 * ec * alkali.gf[k]/alkali.Z * dens
                     * np.exp(-pc.C2*np.abs(pyrat.spec.wn-alkali.wn[k])
                              / np.expand_dims(temp,axis=1)))
+      # Note this equation is neglecting the exp(-Elow/T)*(1-exp(-nu0/T))
+      # terms because they as approximately 1.0 at T=[100K--4000K]
 
     # Sum alkali extinction coefficient (cm-1):
     pyrat.alkali.ec += alkali.ec
