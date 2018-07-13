4c141ff4a8ef2a3ca673fb9e2bd9d63b44f29527
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2018-07-13 16:21:02 +0200

    Bumped Pyrat Bay to version 0.0.89.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -6,7 +6,7 @@
 # Pyrat-Bay version:
 PBAY_VER  =  0  # Major version
 PBAY_MIN  =  0  # Minor version
-PBAY_REV  = 88  # Revision
+PBAY_REV  = 89  # Revision
 
 # Pyrat version:
 PYRAT_VER =  1  # Major version
