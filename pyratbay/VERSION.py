8c88b9972fea7e1909387e7c0e0f2c347327a692
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2019-03-11 17:17:56 +0100

    The next merge enable travis CI testing.  Most of the code works in Python2 and Python3 (some tli's are pending). This merits a new minor version.
    Bumped Pyrat Bay to version 0.1.0.
    Bumped Pyrat to version 1.3.7.
    Bumped lineread to version 6.4.16.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -5,15 +5,15 @@
 
 # Pyrat-Bay version:
 PBAY_VER  =  0  # Major version
-PBAY_MIN  =  0  # Minor version
-PBAY_REV  = 97  # Revision
+PBAY_MIN  =  1  # Minor version
+PBAY_REV  =  0  # Revision
 
 # Pyrat version:
 PYRAT_VER =  1  # Major version
 PYRAT_MIN =  3  # Minor version
-PYRAT_REV =  6  # Revision
+PYRAT_REV =  7  # Revision
 
 # Lineread version:
 LR_VER    =  6  # Major version
 LR_MIN    =  4  # Minor version
-LR_REV    = 15  # Revision
+LR_REV    = 16  # Revision
