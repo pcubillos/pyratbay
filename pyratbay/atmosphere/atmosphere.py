Fixed 'correction' of hydro_m() function when the model diverges for super-puff atmospheres. Print radius array using user-input radius units. Added test of ultra-puff hydro_m() calculation.

pyratbay/atmosphere/atmosphere.py
@@ -419,6 +419,12 @@ def hydro_m(pressure, temperature, mu, mass, p0, r0):
     radius: 1D float ndarray
         Radius for each layer (in cm).
 
+    Notes
+    -----
+    It is possible that this hydrostatic solution diverges when an
+    atmosphere is too puffy.  In such cases, some returned radii
+    will have np.inf values (at the top layers).
+
     Examples
     --------
     >>> import pyratbay.atmosphere as pa
@@ -446,11 +452,10 @@ def hydro_m(pressure, temperature, mu, mass, p0, r0):
     # Set: radius(p0) = r0
     radius = 1.0/(I - I0 + 1/r0)
 
-    # Search for blips (radius should be monotonically decreasing):
+    # Divergent profile (radius should be monotonically decreasing):
     for j in range(len(radius)-1):
         if radius[j] <= radius[j+1]:
-            #radius[0:j+1] = np.inf  # TBD: this shoud be the solution
-            radius[0:j+1] += 2*(radius[j+1] - radius[j])
+            radius[0:j+1] = np.inf
 
     return radius
 

pyratbay/pyrat/makesample.py
@@ -179,8 +179,8 @@ def make_atmprofiles(pyrat):
 
     # Check if Hydrostatic Eq. breaks down:
     ibreak = 0  # Index and flag at the same time
-    if np.any(np.ediff1d(atm_in.radius) > 0.0):
-        ibreak = np.where(np.ediff1d(atm_in.radius)>0)[0][0] + 1
+    if atm_in.radius is not None and np.any(np.isinf(atm_in.radius)):
+        ibreak = np.where(np.isfinite(atm_in.radius))[0][0]
 
     # Set the interpolating function (for use later):
     try:
@@ -279,19 +279,20 @@ def make_atmprofiles(pyrat):
     if atm.radius is not None:
         atm.rtop = pt.ifirst(atm.radius < pyrat.phy.rhill, default_ret=0)
     if atm.rtop > 0:
+        rhill = pyrat.phy.rhill/runits
         log.warning(
             'The atmospheric pressure array extends beyond the Hill radius '
-           f'({pyrat.phy.rhill/pc.km:.1f} km) at pressure '
+           f'({rhill:.5f} {atm.runits}) at pressure '
            f'{atm_in.press[atm.rtop]/pc.bar:.2e} bar (layer {atm.rtop}).  '
             'Extinction beyond this layer will be neglected.')
 
     # Print radius array:
     if atm.radius is not None:
-        radstr = ', '.join(f'{k:9.2f}' for k in atm.radius/pc.km)
-        log.msg(f'Radius array (km) =   [{radstr}]', indent=2, si=4)
+        radstr = ', '.join(f'{k:.3f}' for k in atm.radius/runits)
+        log.msg(f'Radius array ({atm.runits}) =   [{radstr}]', indent=2, si=4)
         log.msg(
             'Valid upper/lower radius boundaries:    '
-           f'{atm.radius[atm.rtop]/runits:8.1f} - {atm.radius[-1]/runits:8.1f} '
+           f'{atm.radius[atm.rtop]/runits:.5f} - {atm.radius[-1]/runits:.5f} '
            f'{atm.runits}.', indent=2)
 
     log.msg(