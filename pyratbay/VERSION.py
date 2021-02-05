f17f40d4d8805546daaf8af0130543c461b302aa
pcubillos <patricio.cubillos@oeaw.ac.at>
2021-02-05 20:38:24 +0100

    Bumped version to 0.9.2.

--- pyratbay/VERSION.py
+++ pyratbay/VERSION.py
@@ -1,17 +1,15 @@
 # Copyright (c) 2016-2021 Patricio Cubillos.
 # Pyrat Bay is open-source software under the GNU GPL-2.0 license (see LICENSE).
 
-# Software versioning:
-
 # Pyrat-Bay version:
 PBAY_VER  =  0  # Major version
 PBAY_MIN  =  9  # Minor version
-PBAY_REV  =  1  # Revision
+PBAY_REV  =  2  # Revision
 
 # Lineread version:
 LR_VER    =  6  # Major version
 LR_MIN    =  5  # Minor version
 LR_REV    =  0  # Revision
 
-__version__ = '{}.{}.{}'.format(PBAY_VER, PBAY_MIN, PBAY_REV)
+__version__ = f'{PBAY_VER}.{PBAY_MIN}.{PBAY_REV}'
 
