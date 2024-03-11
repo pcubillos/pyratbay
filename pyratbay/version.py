69f1beb3469895749e170ab0a572855b07c3d962
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-03-11 16:39:02 +0100

    Updated pyratbay version to 2.0.0beta1. This is it, no more new implementations beyond whats already planned. Only cleaning up and documenting from now on.

--- pyratbay/version.py
+++ pyratbay/version.py
@@ -2,9 +2,9 @@
 # Pyrat Bay is open-source software under the GPL-2.0 license (see LICENSE)
 
 # Pyrat-Bay version:
-PBAY_VER = 1  # Major version
-PBAY_MIN = 1  # Minor version
-PBAY_REV = '15'  # Revision
+PBAY_VER = 2  # Major version
+PBAY_MIN = 0  # Minor version
+PBAY_REV = '0.b1'  # Revision
 
 
 # Lineread version:
