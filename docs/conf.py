c744f2d7b3bf133e971c98179e2b73d1043ba6bc
pcubillos <patricio.cubillos@oeaw.ac.at>
2024-06-07 16:04:30 +0200

    Added sphinx_copybutton to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'IPython.sphinxext.ipython_console_highlighting',
     'nbsphinx',
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
