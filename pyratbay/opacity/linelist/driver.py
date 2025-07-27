790aa6fba08b7445b7c0b402eadc6a12f819b0d8
pcubillos <patricio.cubillos@oeaw.ac.at>
2025-07-27 22:45:51 +0200

    PEP8 cosmetics on linelist classes.

--- pyratbay/opacity/linelist/driver.py
+++ pyratbay/opacity/linelist/driver.py
@@ -9,165 +9,155 @@ from .. import partitions as pf
 
 
 class Linelist():
-  def __init__(self, dbfile, pffile, log):
-      self.dbfile = dbfile
-      self.pffile = pffile
-      self.log = log
+    def __init__(self, dbfile, pffile, log):
+        self.dbfile = dbfile
+        self.pffile = pffile
+        self.log = log
 
 
-  def getpf(self, verbose=0):
-      """
-      Compute partition function for specified source.
+    def getpf(self, verbose=0):
+        """
+        Compute partition function for specified source.
 
-      Returns
-      -------
-      temp: 1D float ndarray
-          Array with temperature sample.
-      PF: 2D float ndarray
-          The partition function data for each isotope at each temperature.
-      isotopes: List of strings
-          The names of the tabulated isotopes
-      """
-      # Calculate the partition-function from the TIPS module:
-      if self.pffile == 'tips':
-          pf_data, isotopes, temp = pf.tips(self.molecule)
-          return temp, pf_data, isotopes
+        Returns
+        -------
+        temp: 1D float ndarray
+            Array with temperature sample.
+        PF: 2D float ndarray
+            The partition function data for each isotope at each temperature.
+        isotopes: List of strings
+            The names of the tabulated isotopes
+        """
+        # Calculate the partition-function from the TIPS module:
+        if self.pffile == 'tips':
+            pf_data, isotopes, temp = pf.tips(self.molecule)
+            return temp, pf_data, isotopes
 
-      # Use polynomial expression:
-      elif self.pffile == 'poly':
-          temp = np.arange(1000.0, 7001.0, 50.0)
-          ntemp = len(temp)
-          niso  = len(self.isotopes)
+        # Use polynomial expression:
+        elif self.pffile == 'poly':
+            temp = np.arange(1000.0, 7001.0, 50.0)
+            ntemp = len(temp)
+            niso  = len(self.isotopes)
 
-          pf_data = np.zeros((niso, ntemp), np.double)
-          for j in range(niso):
-              for i in range(ntemp):
-                  # Formula from Irwin 1981, ApJS 45, 621 (equation #2):
-                  pf_data[j,i] = (
-                      self.PFcoeffs[j,0] +
-                      self.PFcoeffs[j,1]* np.log(temp[i]) +
-                      self.PFcoeffs[j,2]*(np.log(temp[i]))**2 +
-                      self.PFcoeffs[j,3]*(np.log(temp[i]))**3 +
-                      self.PFcoeffs[j,4]*(np.log(temp[i]))**4 +
-                      self.PFcoeffs[j,5]*(np.log(temp[i]))**5
-                  )
-          # Get the exponential of log(PF):
-          pf_data = np.exp(pf_data)
-          return temp, pf_data, self.isotopes
+            pf_data = np.zeros((niso, ntemp), np.double)
+            for j in range(niso):
+                for i in range(ntemp):
+                    # Formula from Irwin 1981, ApJS 45, 621 (equation #2):
+                    pf_data[j,i] = (
+                        self.PFcoeffs[j,0] +
+                        self.PFcoeffs[j,1]* np.log(temp[i]) +
+                        self.PFcoeffs[j,2]*(np.log(temp[i]))**2 +
+                        self.PFcoeffs[j,3]*(np.log(temp[i]))**3 +
+                        self.PFcoeffs[j,4]*(np.log(temp[i]))**4 +
+                        self.PFcoeffs[j,5]*(np.log(temp[i]))**5
+                    )
+            # Get the exponential of log(PF):
+            pf_data = np.exp(pf_data)
+            return temp, pf_data, self.isotopes
 
-      # Extract the partition-function from a tabulated file
-      else:
-          # TBD: Catch file not found error with self.log
-          pf_data, iso, temp = io.read_pf(self.pffile)
-          return temp, pf_data, iso.tolist()
-
-
-  def dbread(self, iwl, fwl, verb):
-      """
-      Read linelist values for specific database type.
-      """
-      pass
+        # Extract the partition-function from a tabulated file
+        else:
+            # TBD: Catch file not found error with self.log
+            pf_data, iso, temp = io.read_pf(self.pffile)
+            return temp, pf_data, iso.tolist()
 
 
-  def readwave(self, dbfile, irec):
-      """
-      Read the wavelength parameter as given in each database.
-      """
-      pass
+    def dbread(self, iwl, fwl, verb):
+        """
+        Read linelist values for specific database type.
+        """
+        pass
 
 
-  def binsearch(self, dbfile, wave, ilo, ihi, searchup=True):
-      """
-      Do a binary (and then linear) search for wavelength/wavenumber in
-      file 'dbfile' between record positions ilo and ihi.
-
-      Parameters
-      ----------
-      dbfile: File object
-         File where to search.
-      wave: Scalar
-         Target wavelength/wavenumber (as given in each specific database).
-      ilo: Integer
-         Lowest index record to search.
-      ihi: Integer
-         highest index record to search.
-      searchup: Boolean
-         Search up (True) or down (False) the records for duplicate results
-         after the binary search.
-
-      Returns:
-      --------
-      irec:  Integer
-         Record index for wave.
-      """
-      # Minimum and maximum boundaries where to search:
-      imin, imax = ilo, ihi
-
-      # Wavelength/wavenumber of record:
-      recwave = 0
-
-      # Binary search:
-      while ihi - ilo > 1:
-          # Middle record index:
-          irec = (ihi + ilo)//2
-          # Read wavelength/wavenumber, depending on linelist format:
-          recwave = self.readwave(dbfile, irec)
-          # Update search limits:
-          if recwave > wave:
-              ihi = irec
-          else:
-              ilo = irec
-
-      # Linear search:
-      if searchup:
-          irec = ilo
-      else:
-          irec = ihi
-
-      icheck  = irec  # Record index to check
-      bounded = True  # Record's wavelength is still within boundaries
-
-      while bounded:
-          irec = icheck
-          # Check index boundaries:
-          if irec == imin or irec == imax:
-              break
-          if searchup:
-              icheck += 1
-              bounded = self.readwave(dbfile, icheck) < wave
-          else:
-              icheck -= 1
-              bounded = self.readwave(dbfile, icheck) > wave
-
-      return irec
+    def readwave(self, dbfile, irec):
+        """
+        Read the wavelength parameter as given in each database.
+        """
+        pass
 
 
-  def get_iso(self, molname):
-      """
-      Get isotopic info from isotopes.dat file.
+    def binsearch(self, dbfile, wave, ilo, ihi, searchup=True):
+        """
+        Do a binary (and then linear) search for wavelength/wavenumber in
+        file 'dbfile' between record positions ilo and ihi.
 
-      Parameters
-      ----------
-      mol: String
-          If not None, extract data based on this molecule name.
-      dbtype: String
-          Database type (for isotope names).
+        Parameters
+        ----------
+        dbfile: File object
+           File where to search.
+        wave: Scalar
+           Target wavelength/wavenumber (as given in each specific database).
+        ilo: Integer
+           Lowest index record to search.
+        ihi: Integer
+           highest index record to search.
+        searchup: Boolean
+           Search up (True) or down (False) the records for duplicate results
+           after the binary search.
 
-      Returns
-      -------
-      isotopes: List of strings
-          Isotopes names.
-      mass: List of floats
-          Masses for each isotope.
-      isoratio: List of integers
-          Isotopic terrestrial abundance ratio.
-      """
-      iso_data = io.read_isotopes(ROOT + 'pyratbay/data/isotopes.dat')
-      name, hit_iso, exo_iso, iso_ratio, iso_mass = iso_data
-      isotopes = exo_iso
+        Returns:
+        --------
+        irec:  Integer
+           Record index for wave.
+        """
+        # Minimum and maximum boundaries where to search:
+        imin, imax = ilo, ihi
 
-      isotopes = [str(iso) for mol,iso in zip(name, isotopes) if mol==molname]
-      mass = [m for mol,m in zip(name, iso_mass) if mol==molname]
-      isoratio = [ratio for mol,ratio in zip(name, iso_ratio) if mol==molname]
+        # Binary search:
+        recwave = 0
+        while ihi - ilo > 1:
+            irec = (ihi + ilo)//2
+            recwave = self.readwave(dbfile, irec)
+            if recwave > wave:
+                ihi = irec
+            else:
+                ilo = irec
 
-      return isotopes, mass, isoratio
+        # Then, linear search (values can be repeated):
+        irec = ilo if searchup else ihi
+        icheck  = irec
+        bounded = True
+
+        while bounded:
+            irec = icheck
+            if irec == imin or irec == imax:
+                break
+            if searchup:
+                icheck += 1
+                bounded = self.readwave(dbfile, icheck) < wave
+            else:
+                icheck -= 1
+                bounded = self.readwave(dbfile, icheck) > wave
+
+        return irec
+
+
+    def get_iso(self, molname):
+        """
+        Get isotopic info from isotopes.dat file.
+
+        Parameters
+        ----------
+        mol: String
+            If not None, extract data based on this molecule name.
+        dbtype: String
+            Database type (for isotope names).
+
+        Returns
+        -------
+        isotopes: List of strings
+            Isotopes names.
+        mass: List of floats
+            Masses for each isotope.
+        isoratio: List of integers
+            Isotopic terrestrial abundance ratio.
+        """
+        iso_data = io.read_isotopes(ROOT + 'pyratbay/data/isotopes.dat')
+        name, hit_iso, exo_iso, iso_ratio, iso_mass = iso_data
+        isotopes = exo_iso
+
+        isotopes = [str(iso) for mol,iso in zip(name, isotopes) if mol==molname]
+        mass = [m for mol,m in zip(name, iso_mass) if mol==molname]
+        isoratio = [ratio for mol,ratio in zip(name, iso_ratio) if mol==molname]
+
+        return isotopes, mass, isoratio
--- pyratbay/opacity/linelist/repack.py
+++ pyratbay/opacity/linelist/repack.py
@@ -14,155 +14,156 @@ from .driver import Linelist
 
 
 class Repack(Linelist):
-  """Repack database reader."""
-  def __init__(self, dbfile, pffile, log):
-      """
-      Initialize Exomol database object.
+    """Repack database reader."""
+    def __init__(self, dbfile, pffile, log):
+        """
+        Initialize Exomol database object.
 
-      Parameters
-      ----------
-      dbfile: String
-          File with the Database line-transition info.
-      pffile: String
-          File with the partition function.
-      log: Log object
-          An mc3.utils.Log instance to log screen outputs to file.
-      """
-      super(Repack, self).__init__(dbfile, pffile, log)
+        Parameters
+        ----------
+        dbfile: String
+            File with the Database line-transition info.
+        pffile: String
+            File with the partition function.
+        log: Log object
+            An mc3.utils.Log instance to log screen outputs to file.
+        """
+        super(Repack, self).__init__(dbfile, pffile, log)
 
-      self.fmt = 'dddi'
-      self.recsize = struct.calcsize(self.fmt)
-      self.dsize = struct.calcsize('d')
+        self.fmt = 'dddi'
+        self.recsize = struct.calcsize(self.fmt)
+        self.dsize = struct.calcsize('d')
 
-      # Get info from file name:
-      self.molecule, self.dbtype = os.path.split(dbfile)[1].split('_')[0:2]
-      self.name = f'repack {self.dbtype} {self.molecule}'
+        # Get info from file name:
+        self.molecule, self.dbtype = os.path.split(dbfile)[1].split('_')[0:2]
+        self.name = f'repack {self.dbtype} {self.molecule}'
 
-      # Get isotopic info:
-      isotopes, mass, ratio = self.get_iso(self.molecule)
-      self.isotopes = isotopes
-      self.mass = mass
-      self.isoratio = ratio
-
-
-  def readwave(self, dbfile, irec):
-      """
-      Read irec-th wavenumber record from FILE dbfile.
-
-      Parameters
-      ----------
-      dbfile: File object
-          File where to extract the wavenumber.
-      irec: Integer
-          Index of record.
-
-      Returns
-      -------
-      wavenumber: Float
-          Wavenumber value in cm-1.
-      """
-      dbfile.seek(irec*self.recsize)
-      wavenumber = struct.unpack('d', dbfile.read(self.dsize))[0]
-      return wavenumber
+        # Get isotopic info:
+        isotopes, mass, ratio = self.get_iso(self.molecule)
+        self.isotopes = isotopes
+        self.mass = mass
+        self.isoratio = ratio
 
 
-  def dbread(self, iwn, fwn, verb):
-      """
-      Read line-transition info between wavenumbers iwn and fwn.
+    def readwave(self, dbfile, irec):
+        """
+        Read irec-th wavenumber record from FILE dbfile.
 
-      Parameters
-      ----------
-      iwn: Float
-          Lower wavenumber boundary in cm-1.
-      fwn: Float
-          Upper wavenumber boundary in cm-1.
-      verb: Integer
-          Verbosity threshold.
+        Parameters
+        ----------
+        dbfile: File object
+            File where to extract the wavenumber.
+        irec: Integer
+            Index of record.
 
-      Returns
-      -------
-      wnumber: 1D float ndarray
-          Line-transition central wavenumber (cm-1).
-      gf: 1D float ndarray
-          gf value (unitless).
-      elow: 1D float ndarray
-          Lower-state energy (cm-1).
-      isoID: 1D integer ndarray
-          Isotope index.
-      """
-      # Open file for reading:
-      data = open(self.dbfile, 'rb')
-      # Get Total number of transitions in file:
-      data.seek(0, 2)
-      nlines = data.tell() // self.recsize
+        Returns
+        -------
+        wavenumber: Float
+            Wavenumber value in cm-1.
+        """
+        dbfile.seek(irec*self.recsize)
+        wavenumber = struct.unpack('d', dbfile.read(self.dsize))[0]
+        return wavenumber
 
-      # Check non-overlaping ranges:
-      DBiwn = self.readwave(data, 0)
-      DBfwn = self.readwave(data, nlines-1)
-      if iwn > DBfwn or fwn < DBiwn:
-          self.log.warning(
-              f"Database ('{os.path.basename(self.dbfile)}') wavenumber "
-              f"range ({DBiwn:.2f}--{DBfwn:.2f} cm-1) does not overlap with "
-              f"the requested wavenumber range ({iwn:.2f}--{fwn:.2f} cm-1).")
-          return None
 
-      # Find the record index for iwn and fwn:
-      istart = self.binsearch(data, iwn, 0,      nlines-1, 0)
-      istop  = self.binsearch(data, fwn, istart, nlines-1, 1)
-      # Number of records to read:
-      nread = istop - istart + 1
+    def dbread(self, iwn, fwn, verb):
+        """
+        Read line-transition info between wavenumbers iwn and fwn.
 
-      # Allocate arrays for values to extract:
-      wnumber = np.zeros(nread, np.double)
-      elow    = np.zeros(nread, np.double)
-      gf      = np.zeros(nread, np.double)
-      iso     = np.zeros(nread, int)
+        Parameters
+        ----------
+        iwn: Float
+            Lower wavenumber boundary in cm-1.
+        fwn: Float
+            Upper wavenumber boundary in cm-1.
+        verb: Integer
+            Verbosity threshold.
 
-      self.log.msg(f'Process {self.name} database between records '
-          f'{istart:,d} and {istop:,d}.', indent=2)
+        Returns
+        -------
+        wnumber: 1D float ndarray
+            Line-transition central wavenumber (cm-1).
+        gf: 1D float ndarray
+            gf value (unitless).
+        elow: 1D float ndarray
+            Lower-state energy (cm-1).
+        isoID: 1D integer ndarray
+            Isotope index.
+        """
+        # Open file for reading:
+        data = open(self.dbfile, 'rb')
+        # Get Total number of transitions in file:
+        data.seek(0, 2)
+        nlines = data.tell() // self.recsize
 
-      interval = (istop - istart) // 10  # Check-point interval
-      if interval == 0:
-          interval = 1
+        # Check non-overlaping ranges:
+        DBiwn = self.readwave(data, 0)
+        DBfwn = self.readwave(data, nlines-1)
+        if iwn > DBfwn or fwn < DBiwn:
+            self.log.warning(
+                f"Database ('{os.path.basename(self.dbfile)}') wavenumber "
+                f"range ({DBiwn:.2f}--{DBfwn:.2f} cm-1) does not overlap with "
+                f"the requested wavenumber range ({iwn:.2f}--{fwn:.2f} cm-1).")
+            return None
 
-      i = 0  # Stored record index
-      while (i < nread):
-          # Read a record:
-          data.seek((istart+i) * self.recsize)
-          wnumber[i], elow[i], gf[i], iso[i] = \
-              struct.unpack(self.fmt, data.read(self.recsize))
-          # Print a checkpoint statement every 10% interval:
-          if (i % interval) == 0.0 and i != 0:
-              self.log.msg(f'{10*i/interval:5.1f}% completed.', indent=3)
-              self.log.debug(
-                  f'Wavenumber: {wnumber[i]:8.2f} cm-1   '
-                  f'Wavelength: {1.0/(wnumber[i]*pc.um):6.3f} um\n'
-                  f'Elow:     {elow[i]:.4e} cm-1   gf: {gf[i]:.4e}   '
-                  f'Iso ID: {iso[i]:2d}', indent=6)
-          i += 1
-      data.close()
+        # Find the record index for iwn and fwn:
+        istart = self.binsearch(data, iwn, 0, nlines-1, 0)
+        istop = self.binsearch(data, fwn, istart, nlines-1, 1)
+        # Number of records to read:
+        nread = istop - istart + 1
 
-      # Unique isotopes and inverse indices:
-      unique_iso, inverse = np.unique(iso, return_inverse=True)
-      # Convert integer to string (care to pad leading '0's):
-      iso_len = len(self.isotopes[0])
-      unique_iso = [str(iso).zfill(iso_len) for iso in unique_iso]
-      n_iso = len(unique_iso)
-      idx = np.zeros(n_iso, int)
-      missing_isotopes = []
-      for i,iso in enumerate(unique_iso):
-          if iso in self.isotopes:
-              idx[i] = self.isotopes.index(iso)
-          else:
-              missing_isotopes.append(iso)
+        # Allocate arrays for values to extract:
+        wnumber = np.zeros(nread, np.double)
+        elow = np.zeros(nread, np.double)
+        gf = np.zeros(nread, np.double)
+        iso = np.zeros(nread, int)
 
-      if len(missing_isotopes) > 0:
-          raise ValueError (
-              f'Unrecognized isotope names for {self.molecule} line-list: '
-              f'{missing_isotopes}\nSee list of known isotopes at '
-              f'{pc.ROOT}pyratbay/data/isotopes.dat'
-          )
+        self.log.msg(f'Process {self.name} database between records '
+            f'{istart:,d} and {istop:,d}.', indent=2)
 
-      isoID = idx[inverse]
+        interval = (istop - istart) // 10  # Check-point interval
+        if interval == 0:
+            interval = 1
 
-      return wnumber, gf, elow, isoID
+        i = 0  # Stored record index
+        while (i < nread):
+            # Read a record:
+            data.seek((istart+i) * self.recsize)
+            wnumber[i], elow[i], gf[i], iso[i] = struct.unpack(
+                self.fmt, data.read(self.recsize),
+            )
+            # Print a checkpoint statement every 10% interval:
+            if (i % interval) == 0.0 and i != 0:
+                self.log.msg(f'{10*i/interval:5.1f}% completed.', indent=3)
+                self.log.debug(
+                    f'Wavenumber: {wnumber[i]:8.2f} cm-1   '
+                    f'Wavelength: {1.0/(wnumber[i]*pc.um):6.3f} um\n'
+                    f'Elow:     {elow[i]:.4e} cm-1   gf: {gf[i]:.4e}   '
+                    f'Iso ID: {iso[i]:2d}', indent=6)
+            i += 1
+        data.close()
+
+        # Unique isotopes and inverse indices:
+        unique_iso, inverse = np.unique(iso, return_inverse=True)
+        # Convert integer to string (care to pad leading '0's):
+        iso_len = len(self.isotopes[0])
+        unique_iso = [str(iso).zfill(iso_len) for iso in unique_iso]
+        n_iso = len(unique_iso)
+        idx = np.zeros(n_iso, int)
+        missing_isotopes = []
+        for i,iso in enumerate(unique_iso):
+            if iso in self.isotopes:
+                idx[i] = self.isotopes.index(iso)
+            else:
+                missing_isotopes.append(iso)
+
+        if len(missing_isotopes) > 0:
+            raise ValueError (
+                f'Unrecognized isotope names for {self.molecule} line-list: '
+                f'{missing_isotopes}\nSee list of known isotopes at '
+                f'{pc.ROOT}pyratbay/data/isotopes.dat'
+            )
+
+        isoID = idx[inverse]
+
+        return wnumber, gf, elow, isoID
