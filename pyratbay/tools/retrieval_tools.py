Fixed bug in Loglike().  Modified data/uncerts from eval() were not being taken into account for likelihood calculation. Updated/added tests. data/uncert modifiers currently only apply to low-res data. Updated test cfg files that were using filters inputs with obsfile. Removed most test filters (shifting toward obsfile definition)

pyratbay/tools/retrieval_tools.py
@@ -42,27 +42,20 @@ class Loglike():
 
         self.params = pyrat.ret.params
         self.pstep = pyrat.ret.pstep
-        self.ifree = self.pstep>0
+        self.ifree = self.pstep > 0
         self.ishare = np.where(self.pstep<0)[0]
-        # Concatenate (low-res) data and high-res data arrays
-        self.data = []
-        self.uncert = []
-        if pyrat.obs.data is not None:
-            self.data = pyrat.obs.data
-            self.uncert = pyrat.obs.uncert
-        if pyrat.obs.data_hires is not None:
-            self.data = np.concatenate((self.data, pyrat.obs.data_hires))
-            self.uncert = np.concatenate((self.uncert, pyrat.obs.uncert_hires))
+
+        if pyrat.obs.data is None and pyrat.obs.data_hires is None:
+            raise ValueError(
+                'Attempting to compute a log-likelihood for a model '
+                'with no data'
+            )
         if np.sum(self.ifree) == 0:
             raise ValueError(
                 'Attempting to compute a log-likelihood for a model '
                 'with no free parameters'
             )
-        if len(self.data) == 0:
-            raise ValueError(
-                'Attempting to compute a log-likelihood for a model '
-                'with no data'
-            )
+
         self.pnames = np.array(pyrat.ret.texnames)
         self.retrieval_file = pyrat.ret.retrieval_file
         # dt_snapshot hours to seconds
@@ -70,6 +63,21 @@ class Loglike():
         if self._dt_snapshot > 0:
             self.timer = time.time()
 
+    def get_data(self):
+        """
+        Concatenate low- and high-resolution data into a single array
+        """
+        obs = self.obs
+        if obs.data is not None and obs.data_hires is not None:
+            data = np.concatenate((obs.data, obs.data_hires))
+            uncert = np.concatenate((obs.uncert, obs.uncert_hires))
+        elif obs.data is not None:
+            data = obs.data
+            uncert = obs.uncert
+        else:
+            data = obs.data_hires
+            uncert = obs.uncert_hires
+        return data, uncert
 
     def __call__(self, params):
         """
@@ -94,10 +102,12 @@ class Loglike():
 
         # Evaluate model (and update data if necessary)
         model = self.func(self.params, retmodel=False)
+        # Concatenate (low-res) data and high-res data arrays
+        data, uncert = self.get_data()
 
         log_like = (
-            -0.5*np.sum(((self.data - model) / self.uncert)**2.0)
-            -0.5*np.sum(np.log(2.0*np.pi*self.uncert**2.0))
+            -0.5*np.sum(((data - model) / uncert)**2.0)
+            -0.5*np.sum(np.log(2.0*np.pi*uncert**2.0))
         )
         if not np.isfinite(log_like):
             log_like = -1.0e98
@@ -344,8 +354,7 @@ def multinest_run(pyrat, basename):
 
     # Statistics:
     best_model = pyrat.eval(bestp, retmodel=False)
-    data = loglike.data
-    uncert = loglike.uncert
+    data, uncert = loglike.get_data()
 
     ndata = len(data)
     best_chisq = np.sum((best_model-data)**2 / uncert**2)

tests/test_tools.py
@@ -689,7 +689,7 @@ def test_interpolate_opacity_extrapolate():
     relative_diff = interp_cs[:,p_mask]/cs[:,0:1]
     expected_diff = np.ones_like(relative_diff)
     np.testing.assert_allclose(relative_diff, expected_diff, rtol=1e-8)
-    
+
 
 def test_none_div_no_num():
     div = pt.none_div(None, 1.0)
@@ -919,27 +919,36 @@ def test_get_multinest_map_2modes():
     np.testing.assert_allclose(bestp, expected_bestp)
 
 
-def test_loglike():
-    pyrat = pb.Pyrat('configs/mcmc_transmission_test.cfg', log=False)
+def test_loglike_base():
+    pyrat = pb.Pyrat('configs/retrieval_transmission_test.cfg', log=False)
     loglike = pt.Loglike(pyrat)
 
     # For the log_like, free parameters only
-    ifree = pyrat.ret.pstep > 0
-    free_pars = pyrat.ret.params[ifree]
+    free_pars = pyrat.ret.params[pyrat.ret.pstep > 0]
     like = loglike(free_pars)
-    np.testing.assert_allclose(like, -1633.7475526010119)
+    np.testing.assert_allclose(like, -1643.6087514224329)
+
+
+def test_loglike_unphysical():
+    pyrat = pb.Pyrat('configs/retrieval_transmission_test.cfg', log=False)
+    loglike = pt.Loglike(pyrat)
 
     # A non-physical model
+    free_pars = pyrat.ret.params[pyrat.ret.pstep > 0]
     free_pars[3] = -1.0
     like = loglike(free_pars)
     np.testing.assert_allclose(like, -1e+98)
 
-    # Now with better-fitting parameters:
-    # map_pars = np.array([
-    #     -4.4567854, -0.87388636, 1352.3337, -3.3590232, -3.4307885,
-    # ])
-    # like = loglike(map_pars)
-    # np.testing.assert_allclose(like, -1627.5530504136932)
+
+def test_loglike_offsets():
+    pyrat = pb.Pyrat('configs/retrieval_transmission_test.cfg', log=False)
+    loglike = pt.Loglike(pyrat)
+
+    # With offsets (that modify data, not the model)
+    free_pars = pyrat.ret.params[pyrat.ret.pstep > 0]
+    free_pars[5] = 100.0
+    like = loglike(free_pars)
+    np.testing.assert_allclose(like, -1322.830780356435)
 
 
 def test_get_mpi_rank():

tests/test_transmission.py
@@ -426,10 +426,10 @@ def test_transmission_fit(tmp_path):
 
 
 def test_transmission_fit_filters():
-    pyrat = pb.run(ROOT+'tests/configs/spectrum_transmission_filters_test.cfg')
-    model4 = pyrat.eval(pyrat.ret.params, retmodel=True)
-    np.testing.assert_allclose(model4[0], expected['fit4'], rtol=rtol)
-    np.testing.assert_allclose(model4[1], expected['bandflux4'], rtol=rtol)
+    pyrat = pb.run('configs/spectrum_transmission_bands.cfg')
+    spectrum, band_spectrum = pyrat.eval(pyrat.ret.params, retmodel=True)
+    np.testing.assert_allclose(spectrum, expected['fit4'], rtol=rtol)
+    np.testing.assert_allclose(band_spectrum, expected['bandflux4'], rtol=rtol)
 
 
 @pytest.mark.parametrize(